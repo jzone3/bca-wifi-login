3293ed162539cfee72c3ab38daa56624f1b4abd7
jzone3 <jszoneraich@gmail.com>
2012-09-06 15:42:04 -0400

    ip fix

--- auto_login.py
+++ auto_login.py
@@ -1,30 +1,27 @@
 import urllib
 import urllib2
 
-def bca_wifi():
-    try:
-        response=urllib2.urlopen('http://168.229.108.58',timeout=1)
-        return True
-    except urllib2.URLError as err: pass
-    return False
+def get_ip():
+	full_url = 'http://automation.whatismyip.com/n09230945.asp'
+	data = urllib2.urlopen(full_url)
+	return str(data)
 
-if bca_wifi():
-	url = 'https://ccahack.bergen.org/auth/perfigo_validate.jsp'
-	values = {
-		'reqFrom' : 'perfigo_simple_login.jsp',
-		'uri' : 'https://ccahack.bergen.org/',
-		'cm' : 'ws32vklm',
-		'userip' : '168.229.108.58',
-		'os' : 'MAC_OSX',
-		'index' : '4',
-		'username' : 'USERNAME_HERE',
-		'password' : 'PASSWORD_HERE',
-		'provider' : 'BCA',
-		'login_submit' : 'Continue'
-	}
+url = 'https://ccahack.bergen.org/auth/perfigo_validate.jsp'
+values = {
+	'reqFrom' : 'perfigo_simple_login.jsp',
+	'uri' : 'https://ccahack.bergen.org/',
+	'cm' : 'ws32vklm',
+	'userip' : get_ip(),
+	'os' : 'MAC_OSX',
+	'index' : '4',
+	'username' : 'USERNAME_HERE',
+	'password' : 'PASSWORD_HERE',
+	'provider' : 'BCA',
+	'login_submit' : 'Continue'
+}
 
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
-	response = urllib2.urlopen(req)
-	the_page = response.read()
-	print the_page
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+print the_page
